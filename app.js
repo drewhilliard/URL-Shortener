a0cf2a91b678b6a10a380333d2d4fc70da5b77f6
Drew Hilliard <drewhilliard@me.com>
2016-09-29 20:53:05 -0500

    Cleaned up JSON response

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ app.get('/', function(req, res){
 });
 
 app.post('/api/v1/shortening/:url', function(req, res){
+	// Imports the URL shortener module
 	var shorten = require("./modules/shortening.js");
 
+	// Calls the shortenUrl function in the shortener module
 	output = shorten.shortenUrl();
 
+	// Responds with the original input and the new short URL in JSON
 	res.json({"longurl": req.params.url, "shorturl": output});
 });
 
